374be992843d946f21cd3d171d43ed16c54afd90
Sainathplv <sainathreddypalavala7@gmail.com>
2024-12-01 21:12:44 -0600

    homepage-1-first herocontainer

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,27 @@
+import { Link } from "react-router-dom";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import styles from "../styles/Navbar.module.css";
+
+const Navbar = () => {
+  return (
+    <nav className={styles.navbar}>
+      <h1 className={styles.logo}>Flight Finder</h1>
+      <div className={styles.links}>
+        <Link to="/flights">
+          <i className="bi bi-airplane" /> Flights
+        </Link>
+        <Link to="/hotels">
+          <i className="bi bi-building" /> Hotels
+        </Link>
+        <Link to="/vacations">
+          <i className="bi bi-beach" /> Vacations
+        </Link>
+        <Link to="/login">
+          <i className="bi bi-person" /> Login
+        </Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
